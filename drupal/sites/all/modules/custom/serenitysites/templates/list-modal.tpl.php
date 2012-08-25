d6bf2a7d0b91214575a149ad1e12408c9d32d6fe
Mike Dorrell <mike.dorrell@digitaria.com>
2012-08-24 23:40:55 -0700

    added modal list

---
+++ drupal/sites/all/modules/custom/serenitysites/templates/list-modal.tpl.php
@@ -0,0 +1,12 @@
+<!-- Start: List Modal -->
+<div id="<?php print $modal_class_id;?>-list" class="list-modal">
+	<div class="list-modal-header">
+		<h2><?php print $title;?></h2>
+
+		<a href="#" class="list-modal-close"><?php print t('Close');?></a>
+	</div>
+	<div class="list-modal-content">
+    <?php print $content; ?>
+  </div>
+</div>
+<!-- End: List Modal -->
